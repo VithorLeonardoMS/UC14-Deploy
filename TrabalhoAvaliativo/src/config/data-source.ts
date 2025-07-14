cacede1035076d5c095f37f1225f944c63c8cf23
Vithor Soares <vithorleonardomellosoares@gmail.com>
2025-07-14 17:21:20 -0300

    Configurando ambiente de desenvolvimento com sqlite

--- TrabalhoAvaliativo/src/config/data-source.ts
+++ TrabalhoAvaliativo/src/config/data-source.ts
@@ -1,15 +1,47 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import dotenv from "dotenv"
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "database.sqlite",
-    synchronize: true,
-    logging: false,
-    entities: ["src/models/**/*.ts"],
-    migrations: ["src/migrations/**/*.ts"],
-    subscribers: [],
-});
+// Flag para saber se estamos em produção
+const isProduction = process.env.NODE_ENV === "production";
+
+// Variáveis para configuração da conexão
+let dataSourceOptions: DataSourceOptions;
+
+if (isProduction) {
+    
+    // Em produção (ex: Render), usa a variável DATABASE_URL
+
+    dataSourceOptions = {
+        type: "sqlite",
+        database: "database.sqlite",
+        entities: [".//models/*.js"],
+        migrations: [".//migrations/*.js"],
+        synchronize: false,
+        logging: true,
+    };
+} else {
+    
+    // Em desenvolvimento (local), usa variáveis separadas
+    
+    const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+    dataSourceOptions = {
+        type: "mysql",
+        host: DB_HOST,
+        port: Number(DB_PORT),
+        username: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        entities: ["./src/models/*.ts"],
+        migrations: ["./src/migrations/*.ts"],
+        synchronize: false,
+        logging: true,
+    };
+}
+
+// Criação do DataSource com base nas opções acima
+const AppDataSource = new DataSource(dataSourceOptions);
+export default AppDataSource;
